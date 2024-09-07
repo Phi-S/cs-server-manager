30d5df6941b5384eeee22b2c596d1531d55fab14
Phi-S <926151+Phi-S@users.noreply.github.com>
2024-09-07 10:38:39 +0200

    fixed settings validation for SteamLoginToken

--- backend/server/start_parameters.go
+++ backend/server/start_parameters.go
@@ -5,7 +5,7 @@ type StartParameters struct {
 	Password        string   `json:"password" validate:"omitempty,alphanum,lte=32"`
 	StartMap        string   `json:"start_map" validate:"required,printascii,lte=32"`
 	MaxPlayers      uint8    `json:"max_players" validate:"required,number,lte=128"`
-	SteamLoginToken string   `json:"steam_login_token" validate:"omitempty,alphanum,eq=32"`
+	SteamLoginToken string   `json:"steam_login_token" validate:"omitempty,alphanum,len=32"`
 	Additional      []string `json:"additional" validate:"omitempty,dive"`
 }
 
